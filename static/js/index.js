36c3204987c1747c32def686e864f31853f656d7
sajjad <sm89iran@gmail.com>
2026-07-01 08:04:02 +0330

    update header

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+var search_button = document.getElementById("searchbutton");
+var search_box = document.getElementById("searchbox");
+var search_input = document.getElementById("searchinput");
+
+search_button.addEventListener('click' , function(){
+    search_box.style.display = 'block';
+    search_input.focus();
+});
+
+search_input.addEventListener('blur' , function(){
+    search_box.style.display = 'none';
+});
