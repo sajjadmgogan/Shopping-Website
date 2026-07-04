311886940a4f1bc5842d35836d9dcc702ce0140d
sajjad <sm89iran@gmail.com>
2026-07-04 10:53:32 +0330

    creating container and product pox

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,9 @@
 var search_button = document.getElementById("searchbutton");
 var search_box = document.getElementById("searchbox");
 var search_input = document.getElementById("searchinput");
+var product_container1 = document.getElementById("container1");
+var btn_right_pro_con1 = document.getElementById("btn_right")
+var btn_left_pro_con2 = document.getElementById("btn_left")
 
 search_button.addEventListener('click' , function(){
     search_box.style.display = 'block';
@@ -11,3 +14,16 @@ search_input.addEventListener('blur' , function(){
     search_box.style.display = 'none';
 });
 
+btn_right_pro_con1.addEventListener('click' , function(){
+    product_container1.scrollBy({
+        left: 120,
+        behavior: "smooth"
+    });
+});
+
+btn_left_pro_con2.addEventListener('click' , function(){
+    product_container1.scrollBy({
+        left: -120,
+        behavior: "smooth"
+    });
+});
