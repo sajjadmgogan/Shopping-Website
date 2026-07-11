58d0c592a16dec9e5c56a32f0957fcd33b7ec95b
sajjad <sm89iran@gmail.com>
2026-07-11 10:13:43 +0330

    Configuration for various devices_labtap created and tablet creating_menu strip created

--- static/js/index.js
+++ static/js/index.js
@@ -7,6 +7,8 @@ var btn_right_pro_con1 = document.getElementById("btn_right");
 var btn_left_pro_con2 = document.getElementById("btn_left");
 var banner_slider = document.getElementById("banner_container");
 var banners = document.querySelectorAll(".banner");
+var menu_strip_btn = document.getElementById("menufooterimg");
+var menu_strip = document.getElementById("menu_strip");
 var styl = getComputedStyle(banners[0])
 var widths = 0;
 var current = 0;
@@ -42,6 +44,10 @@ setInterval(function(){
     }
 },100)
 
+menu_strip_btn.addEventListener('click' , function(){
+    menu_strip.style.display = "flex";
+})
+
 
 
 search_button.addEventListener('click' , function(){
@@ -56,9 +62,10 @@ search_button_nav.addEventListener('click' , function(){
 
 document.addEventListener("click", function(e){
 
-    if(!search_box.contains(e.target) && e.target !== search_button && e.target !== search_button_nav){
+    if(!search_box.contains(e.target) && !menu_strip.contains(e.target) && e.target !== search_button && e.target !== search_button_nav && e.target !== menu_strip_btn){
 
         search_box.style.display = "none";
+        menu_strip.style.display = "none";
 
     }
 
